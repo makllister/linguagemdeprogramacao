320e6b67bb7754006a8f80f07c9b0abcbf0fe2f4
Makllister <makllister_ml@hotmail.com>
2024-07-16 13:35:22 -0300

    Add files via upload

---
+++ Atividade 15.7/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Atividade15._7.Models;
+using System.Collections.Generic;
+
+namespace Atividade15._7.Controllers
+{
+    public class MessageController : Controller
+    {
+        private static List<Message> messages = new List<Message>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(messages);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Message message)
+        {
+            messages.Add(message);
+            return View(messages);
+        }
+    }
+}
