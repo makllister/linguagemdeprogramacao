8de1e0b9647e21633cf57df5e9cd762b94839a40
Makllister <makllister_ml@hotmail.com>
2024-07-16 13:37:24 -0300

    Add files via upload

---
+++ Atividade 15.7/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade15._7.Models
+{
+    public class Message
+    {
+        public string Name { get; set; }
+       public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+    }
+}
