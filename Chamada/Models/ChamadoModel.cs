a0df4aff6af41fd716c2b8bf6a4d224d51167ec7
Makllister <makllister_ml@hotmail.com>
2024-07-19 16:39:20 -0300

    Add files via upload

---
+++ Chamada/Models/ChamadoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Chamados.Models
+{
+    public class Chamado
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Solicitacao { get; set; } = string.Empty;
+        public string Responsavel { get; set; } = string.Empty;
+        public DateTime Inicio { get; set; }
+        public DateTime? Termino { get; set; }
+
+    }
+}
